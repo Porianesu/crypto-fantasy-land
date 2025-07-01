49bca00776dfd8198c5ddf0571f1ab5a6a829514
Porianesu <976384615@qq.com>
2025-07-01 10:07:01 +0800

    Update: add ts-nocheck to gsapUtils.ts

--- src/utils/gsapUtils.ts
+++ src/utils/gsapUtils.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-nocheck
 import { gsap } from 'gsap'
 import { Draggable } from 'gsap/Draggable'
 import { InertiaPlugin } from 'gsap/InertiaPlugin'
